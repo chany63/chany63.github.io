f81160ff17561bc430780ca0d5014d717d31d7ab
chany63 <chany63@korea.ac.kr>
2023-01-06 16:10:49 +0900

    feat: add category tab and create test post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "First post: test",
+        "excerpt":"this is a test page  ","categories": ["Blog"],
+        "tags": ["Blog","GitHub Page","Git"],
+        "url": "/blog/testpage/",
+        "teaser": null
+      }]
